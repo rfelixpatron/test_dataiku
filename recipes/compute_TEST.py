bcc94f0ed220d519af45ce77191e5de3e572dfb2
roberto.patron@abnamroclearing.com <roberto.patron@abnamroclearing.com>
2024-03-13 11:43:40 +0000

    Saved recipe: GIT_FOR_PROJECTS_FOR_ROBERTO_PATRON_PRUNE_FLOW.compute_TEST

---
+++ recipes/compute_TEST.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+Customers = dataiku.Dataset("Customers")
+Customers_df = Customers.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+TEST_df = Customers_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+TEST = dataiku.Dataset("TEST")
+TEST.write_with_schema(TEST_df)
